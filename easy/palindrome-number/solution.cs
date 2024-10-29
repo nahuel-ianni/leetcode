5c7dbb2fb3a09603df00285ff4a537ae682da1e3
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-10-29 09:35:24 +0100

    Created solution.cs

---
+++ easy/palindrome-number/solution.cs
@@ -0,0 +1,22 @@
+/// This is my solution for the 'Palindrome Number' problem.
+/// Time complexity: O(log(n).
+/// Space complexity: O(1).
+
+public class Solution {
+    public bool IsPalindrome(int number) {
+        if (number < 0)
+            return false;
+
+        var mod = 10;
+        var reversedNumber = 0;
+        var manipulatedNumber = number;
+        
+        while (manipulatedNumber != 0)
+        {
+            reversedNumber = (reversedNumber * mod) + (manipulatedNumber % mod);
+            manipulatedNumber /= mod;
+        }
+
+        return number == reversedNumber;
+    }
+}
