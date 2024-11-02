c8ed4751d777ae7ea8e868973fa0d328eff57be7
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-11-02 06:48:32 +0100

    Create solution.py

---
+++ easy/make-fancy-string/solution.py
@@ -0,0 +1,22 @@
+# This is my solution for the 'Delete characters to make fancy string' problem.
+# Time complexity: O(n).
+# Space complexity: O(n).
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        result = []
+        count = 1
+
+        for i in range(len(s) - 1):
+            if s[i] == s[i + 1]:
+                count += 1
+                
+            else:
+                # Append one or two occurrences based on the count
+                result.append(s[i] * min(count, 2))
+                count = 1  # Reset count for the new character
+
+        # Handle the last character(s)
+        result.append(s[-1] * min(count, 2))
+
+        return ''.join(result)
