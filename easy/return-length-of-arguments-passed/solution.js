8703cbdd80bcad23c38a4c568bb046396ee2e2cd
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-11-08 08:51:02 +0100

    Update solution.js

--- easy/return-length-of-arguments-passed/solution.js
+++ easy/return-length-of-arguments-passed/solution.js
@@ -1,11 +1,14 @@
-/**
+/*
  * Solution for the 'Return length of arguments passed' problem.
  * Time complexity: O(1).
  * Space complexity: O(n).
  *
- * @param {...(null|boolean|number|string|Array|Object)} args - Accepts any number of arguments of various types.
+ * @param {...(null|boolean|number|string|Array|Object)} args - 
+ * Accepts any number of arguments of various types.
+ * 
  * @return {number} - Returns the number of arguments passed.
  */
+
 var argumentsLength = function(...args) {
     // The length property of the 'args' array represents the number of arguments
     return args.length;
