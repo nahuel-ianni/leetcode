ad8699ab4af995153ed74951d1db146cf74d37d9
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-11-10 18:39:22 +0100

    Update solution.cs

--- easy/circular-sentence/solution.cs
+++ easy/circular-sentence/solution.cs
@@ -1,5 +1,5 @@
-/**
- * This is my solution for the 'Circular sentence' problem.
+/*
+ * Solution for the 'Circular sentence' problem.
  * Time complexity: O(n).
  * Space complexity: O(1).
  */
@@ -8,13 +8,14 @@
 {
     public bool IsCircularSentence(string sentence)
     {
-        // Check the first and last characters of the sentence.
+        // Process:
+        //     1. Check if the first and last characters match.
+        //     2. Verify circular condition at each space, ensuring continuity.
         if (sentence[0] != sentence[sentence.Length - 1])
             return false;
 
         for (var x = 1; x < sentence.Length; x++)
         {
-            // Check the circular condition between words at every space.
             if (sentence[x] == ' ' &&
                 sentence[x - 1] != sentence[x + 1])
                     return false;
