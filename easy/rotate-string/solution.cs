e520a0afe93f701a65519b914442cb89502561f5
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-11-10 18:49:08 +0100

    Update solution.cs

--- easy/rotate-string/solution.cs
+++ easy/rotate-string/solution.cs
@@ -1,15 +1,15 @@
-/**
- * This is my solution for the 'Rotate String' problem.
+/*
+ * Solution for the 'Rotate String' problem.
  * Time complexity: O(n).
  * Space complexity: O(n).
- *
- * 1st check: Lengths must be the same if a rotation is to lead to equal strings.
- * 2nd check: By concatenating the string twice, goal should be within. This removes
- *            the need to iterate and exchange chars, reducing time complexity.
  */
 
 public class Solution
 {
     public bool RotateString(string s, string goal) =>
+        /// Process:
+        ///    1. Ensure both strings have the same length.
+        ///    2. Check if goal is a substring within a double concatenation of s.
+        ///       This removes the need to iterate and exchange chars, reducing time complexity.
         s.Length == goal.Length && (s + s).Contains(goal);
 }
