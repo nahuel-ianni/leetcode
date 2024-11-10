085abc4ca34df1c078906ca75ddc1c91f973de42
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2024-11-10 18:44:14 +0100

    Update solution.py

--- easy/length-of-last-word/solution.py
+++ easy/length-of-last-word/solution.py
@@ -1,9 +1,13 @@
-# This is my solution for the 'Length of Last Word' problem.
+# Solution for the 'Length of Last Word' problem.
 # Time complexity: O(n).
 # Space complexity: O(1).
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        # Process:
+        #     1. Remove trailing whitespace from the string.
+        #     2. Traverse characters from the end, counting until reaching a space.
+        
         length = 0
         s = s.rstrip()      # Remove trailing whitespace
 
