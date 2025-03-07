18d94b9ab9f7fe76c5e38a660d1bf2c94fe78528
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-03-07 13:31:53 +0100

    Problem solved

---
+++ easy/article-views-i/solution.sql
@@ -0,0 +1,7 @@
+-- Solution to the 'Article Views I' problem.
+-- Write your PostgreSQL query statement below.
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
