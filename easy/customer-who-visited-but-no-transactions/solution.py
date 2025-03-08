5a122683ea18780f94b6a7d66a07f35e46eeeebc
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-03-08 09:13:28 +0100

    Problem solved

--- easy/customer-who-visited-but-no-transactions/solution.py
+++ easy/customer-who-visited-but-no-transactions/solution.py
@@ -3,6 +3,13 @@
 import pandas as pd
 
 def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
-    df = pd.merge(visits, transactions, on='visit_id', how='left', indicator=True)
-    df = df[df['_merge'] == 'left_only']
-    return df.groupby('customer_id').size().reset_index(name='count_no_trans')
+    missing = visits[~visits['visit_id'].isin(transactions['visit_id'])]
+    grouped = missing.groupby('customer_id',as_index= False)['visit_id'].count()
+    return grouped.rename(columns={'visit_id':'count_no_trans'})
+
+# # Alternative [less-performant] option.
+# # This creates a new DF, increasing memory usage.
+# def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+#     df = pd.merge(visits, transactions, on='visit_id', how='left', indicator=True)
+#     df = df[df['_merge'] == 'left_only']
+#     return df.groupby('customer_id').size().reset_index(name='count_no_trans')
