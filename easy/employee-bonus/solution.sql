02232a6c1688f73e6b726061e719d06f5c444324
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-03-15 18:37:38 +0100

    Problem solved

---
+++ easy/employee-bonus/solution.sql
@@ -0,0 +1,7 @@
+-- Solution to the 'Employee Bonus' problem.
+-- Write your PostgreSQL query statement below.
+
+SELECT E.name, B.bonus
+FROM Employee E
+LEFT JOIN Bonus B ON E.empId = B.empId
+WHERE B.bonus < 1000 OR B.bonus IS NULL;
