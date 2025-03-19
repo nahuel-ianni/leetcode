0ad4661cd628aefbf425ace3679768b55ed89d6c
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-03-19 08:36:43 +0100

    Solved problem

---
+++ easy/find-followers-count/solution.sql
@@ -0,0 +1,7 @@
+-- Solution to the 'Find Followers Count' problem.
+-- Write your PostgreSQL query statement below.
+
+SELECT user_id, COUNT(*) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
