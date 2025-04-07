dfe410e77087ea02faf0674774df0614ed4e4780
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-04-07 18:59:19 +0200

    Solved problem

---
+++ easy/reverse-vowels-of-a-string/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    vowels = set('aeiouAEIOU')
+
+    def is_vowel(self, ch: str) -> bool:
+        return ch in self.vowels
+
+    def reverseVowels(self, s: str) -> str:
+        result = list(s)
+        i, x = 0, len(s) - 1
+        
+        while i < x:
+            while i < x and not self.is_vowel(result[i]):
+                i += 1
+            while i < x and not self.is_vowel(result[x]):
+                x -= 1
+                
+            result[i], result[x] = result[x], result[i]
+            i += 1
+            x -= 1
+        
+        return ''.join(result)
