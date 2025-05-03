d9b2024657279ee39ae773d8008aa541b55d08e6
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-05-03 10:30:20 +0200

    Problem solved

--- easy/patients-with-a-condition/solution.sql
+++ easy/patients-with-a-condition/solution.sql
@@ -3,9 +3,13 @@
 
 SELECT *
 FROM Patients
-WHERE REGEXP_LIKE(conditions, 'DIAB1');
+WHERE conditions ~ '(^| )DIAB1';
+
+-- WHERE conditions LIKE '% DIAB1%' 
+--    OR conditions LIKE 'DIAB1%';
 
 -- The following check ONLY if the first word begins with DIAB1.
+-- WHERE REGEXP_LIKE(conditions, '\mDIAB1');
 -- WHERE LEFT(conditions, 5) = 'DIAB1';
 -- WHERE conditions ^@ 'DIAB1';
 -- WHERE STARTS_WITH(conditions, 'DIAB1');
