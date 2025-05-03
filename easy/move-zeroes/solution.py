93ff2f2bd0d0cde84cd7a429a65f4727eaa1bdb8
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-05-03 15:17:30 +0200

    Improved solution

--- easy/move-zeroes/solution.py
+++ easy/move-zeroes/solution.py
@@ -7,16 +7,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i, num in enumerate(nums):
-            if num != 0:
-                continue
+        last_non_zero = 0
 
-            idx = i
-            while idx < len(nums) - 1 and nums[idx] == 0:
-                idx += 1
-            
-            nums[i] = nums[idx]
-            nums[idx] = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if i != last_non_zero:
+                    nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
+
+                last_non_zero += 1
+
+    
+    # def moveZeroes(self, nums: List[int]) -> None:
+    #     """
+    #     Do not return anything, modify nums in-place instead.
+    #     Same complexities but more irrelevant exchanges.
+    #     """
+    #     for i, num in enumerate(nums):
+    #         if num != 0:
+    #             continue
+
+    #         idx = i
+    #         while idx < len(nums) - 1 and nums[idx] == 0:
+    #             idx += 1
+    #       
+    #         nums[i] = nums[idx]
+    #         nums[idx] = 0
 
     # def moveZeroes(self, nums: List[int]) -> None:
     #     """
