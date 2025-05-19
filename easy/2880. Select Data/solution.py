edf3a8ed4c8d49a3e7e8a45ec2f0b07a78355360
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-05-20 00:59:58 +0200

    Added alternative solution

--- easy/2880. Select Data/solution.py
+++ easy/2880. Select Data/solution.py
@@ -5,4 +5,5 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    return students[students['student_id'] == 101][['name', 'age']]
+    return students.loc[students["student_id"] == 101, ["name","age"]]
+    # return students[students['student_id'] == 101][['name', 'age']]
