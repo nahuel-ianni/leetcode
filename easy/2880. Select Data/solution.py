cb4add8982d535922b9aff301026804d32367e48
N. Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2025-05-20 00:57:56 +0200

    Solved problem

---
+++ easy/2880. Select Data/solution.py
@@ -0,0 +1,8 @@
+# Solution for the 'Select Data' problem.
+# Time complexity: O(n).
+# Space complexity: O(n).
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students[students['student_id'] == 101][['name', 'age']]
